0dfc1d90c47c1d6b89d3adc7c4948adf22a8b3c5
Sam Aaron <samaaron@gmail.com>
2010-09-09 16:55:35 +0100

    initial commit

---
+++ src/polynome/core.clj
@@ -0,0 +1,1 @@
+(ns polynome.core)
---
+++ test/polynome/test/core.clj
@@ -0,0 +1,6 @@
+(ns polynome.test.core
+  (:use [polynome.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
