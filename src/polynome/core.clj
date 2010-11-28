2194e0480361fa0358ab088d135abfd07acbd199
Sam Aaron <samaaron@gmail.com>
2010-11-28 21:34:16 +0000

    beef up init fn to allow for different monome types and shortcuts for each. Also add callbacks helper fn.

--- src/polynome/core.clj
+++ src/polynome/core.clj
@@ -8,6 +8,7 @@
 
 (defrecord Event [time x y action])
 
+
 (defn button-state
   [m]
   @(get-in m [::core :button-state]))
@@ -56,6 +57,12 @@
                            time (- (:time release) (:time press))]
                        (f x y time state))))))
 
+(defn callbacks
+  "Return a list of callbacks associated with monome m"
+  [m]
+  @(get-in m [::core :callbacks]))
+
+
 (defn remove-callback
   [m name]
   (println "implement me!"))
@@ -64,53 +71,77 @@
   [m]
   (reset! (get-in m [::core :callbacks]) []))
 
+(defn- detect-type
+  [filename]
+  (condp re-find filename
+    #"-m64-" :64
+    #"-m128-" :128l
+    #"-m256-" :256
+    #"-m512-" :512
+    :64))
+
+(defn- infer-range
+  [type]
+  (case type
+        :64 [8 8]
+        :128  [16 8]
+        :128l [16 8]
+        :128p [8 16]
+        :256  [16 16]
+        :512  [32 16]
+        :512l [32 16]
+        :512p [16 32]))
+
 (defn init  "Initialise a monome. Raises an exception if the supplied path isn't valid or is already in use"
-  [path]
-  (let [m      (monome-core/connect path)
-        max-x 7
-        max-y 7
-        range-x (inc max-x)
-        range-y (inc max-y)
-        coords (for [y (range range-y)
-                     x (range range-x)]
-                 [x y])
+  ([path] (init path (detect-type path)))
+  ([path type] (apply init (cons path (infer-range type))))
+  ([path n-cols n-rows]
+     (let [m      (monome-core/connect path)
+           max-x (dec n-cols)
+           max-y (dec n-rows)
+           range-x (inc max-x)
+           range-y (inc max-y)
+           coords (for [y (range range-y)
+                        x (range range-x)]
+                    [x y])
 
-        history (list)
-        led-activation    (into {} (map (fn [el] [el :inactive]) coords))
-        button-activation (into {} (map (fn [el] [el :inactive]) coords))
-        press-count       (into {} (map (fn [el] [el 0]) coords))
+           history (list)
+           led-activation    (into {} (map (fn [el] [el :inactive]) coords))
+           button-activation (into {} (map (fn [el] [el :inactive]) coords))
+           press-count       (into {} (map (fn [el] [el 0]) coords))
 
-        callbacks (atom [])
-        led-state    (atom led-activation)
-        button-state (atom {:event-history history
-                            :button-activation button-activation
-                            :led-activation led-activation
-                            :press-count press-count})
+           callbacks (atom [])
+           led-state    (atom led-activation)
+           button-state (atom {:event-history history
+                               :button-activation button-activation
+                               :led-activation led-activation
+                               :press-count press-count})
 
-         poly-m (assoc m ::core {:max-x max-x
-                                :max-y max-y
-                                :range-x range-x
-                                :range-y range-y
-                                :callbacks callbacks
-                                :coords coords
-                                :button-state button-state})
+           poly-m (assoc m ::core {:max-x max-x
+                                   :max-y max-y
+                                   :range-x range-x
+                                   :range-y range-y
+                                   :callbacks callbacks
+                                   :coords coords
+                                   :button-state button-state})
 
-        update-button-state (fn [state action x y]
-                              (let [event (Event. (System/currentTimeMillis) x y action)
-                                    state (update-in state [:event-history] conj event)]
-                                (case action
-                                      :press (-> state
-                                                 (assoc-in [:button-activation [x y]] :active)
-                                                 (update-in [:press-count [x y]] inc))
-                                      :release (-> state
-                                                   (assoc-in [:button-activation [x y]] :inactive)))))
+           update-button-state (fn [state action x y]
+                                 (let [event (Event. (System/currentTimeMillis) x y action)
+                                       state (update-in state [:event-history] conj event)]
+                                   (case action
+                                         :press (-> state
+                                                    (assoc-in [:button-activation [x y]] :active)
+                                                    (update-in [:press-count [x y]] inc))
+                                         :release (-> state
+                                                      (assoc-in [:button-activation [x y]] :inactive)))))
 
-        update-button-state-handler (fn [action x y]
-                                      (let [new-state (swap! button-state update-button-state action x y)]
-                                        (doseq [[_ callback] @callbacks] (callback action x y new-state))))]
+           update-button-state-handler (fn [action x y]
+                                         (let [new-state (swap! button-state update-button-state action x y)]
+                                           (doseq [[_ callback] @callbacks] (callback action x y new-state))))]
 
-    (handlers/on-action poly-m update-button-state-handler ::state "update monome state")
-    poly-m))
+       (handlers/on-action poly-m update-button-state-handler ::state "update monome state")
+       poly-m)))
+
 
 (defn max-x
   "Returns the monome's maximum x coord"
