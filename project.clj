986c1da8f7141a9cb47c2292e95204c632e2e9a9
Sam Aaron <samaaron@gmail.com>
2011-03-15 17:10:46 +0000

    update deps and proj description

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject polynome "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject polynome "0.1.2"
+  :description "A friendly monome API"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [monome-serial "0.1.5"]])
