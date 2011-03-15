dfda454b885254026cad7480aec3aec3cb687127
Sam Aaron <samaaron@gmail.com>
2011-03-15 17:47:24 +0000

    udpate monome-serial dep

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject polynome "0.1.2"
+(defproject polynome "0.1.4"
   :description "A friendly monome API"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [monome-serial "0.1.5"]])
+                 [monome-serial "0.1.6"]])
