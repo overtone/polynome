da84e27ee75ac5924bdc84c1f2fb4eb45a4bdd3c
Sam Aaron <samaaron@gmail.com>
2011-03-15 17:52:16 +0000

    update monome-serial

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject polynome "0.1.4"
+(defproject polynome "0.1.5"
   :description "A friendly monome API"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [monome-serial "0.1.6"]])
+                 [monome-serial "0.2.0"]])
