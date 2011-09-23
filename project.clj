1e6ce9b529963e80504d530c78b40c5f0e1e0fab
Sam Aaron <samaaron@gmail.com>
2011-09-24 00:16:32 +0200

    Depend on Clojure 1.3.0 final

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject polynome "0.2.0"
+(defproject polynome "0.2.1"
   :description "A friendly monome API"
-  :dependencies [[org.clojure/clojure "1.3.0-RC0"]
-                 [monome-serial "0.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [monome-serial "0.3.1"]])
