15a263566674194543833d45e12f996a408996c3
Philip Potter <philipgpotter@gmail.com>
2012-01-11 19:08:30 +0100

    Add tests which would have caught the led-on and led-off bugs

--- test/overtone/test/polynome.clj
+++ test/overtone/test/polynome.clj
@@ -13,15 +13,29 @@
     (fact (max-x polynome) => 14)
     (fact (max-y polynome) => 11)))
 
-(fact "The print-method contains the dimensions of the polynome"
-  (let [polynome (init (make-dummy 15 12))
-        writer   (StringWriter.)]
-    (print-method polynome writer)
-    (.toString writer) => (contains "dimensions[15 12]")))
-
 (facts "The number of coords should be x*y"
   (let [polynome1 (init (make-dummy 10 10))
         polynome2 (init (make-dummy 11 11))]
     (fact (count (coords polynome1)) => 100)
     (fact (count (coords polynome2)) => 121)))
 
+(fact "led-on should update the led-activation state"
+  (let [polynome (init (make-dummy 2 2))]
+    (fact
+      (do (led-on polynome 0 0)
+          (if (await-for 1000 (state-agent polynome))
+            (led-activation polynome)
+            :timed-out))
+      => {[0 0] 1 [0 1] 0 [1 0] 0 [1 1] 0}
+      )))
+
+(fact "led-off should update the led-activation state"
+  (let [polynome (init (make-dummy 2 2))]
+    (fact
+      (do (led-on polynome 0 0)
+          (led-off polynome 0 0)
+          (if (await-for 1000 (state-agent polynome))
+            (led-activation polynome)
+            :timed-out))
+      => {[0 0] 0 [0 1] 0 [1 0] 0 [1 1] 0}
+      )))
